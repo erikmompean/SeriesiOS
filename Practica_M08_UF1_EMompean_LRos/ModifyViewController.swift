ffdd45af166f4eae8fa7328d3c0ef08f68e3cb92
Alumne <Alumne@iMac-de-manteniment-4.local>
2018-03-22 17:41:06 +0100

    ModifyViewControl created

---
+++ Practica_M08_UF1_EMompean_LRos/ModifyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModifyViewController.swift
+//  Practica_M08_UF1_EMompean_LRos
+//
+//  Created by Alumne on 22/3/18.
+//  Copyright © 2018 CFGS La Salle Gracia. All rights reserved.
+//
+
+import UIKit
+
+class ModifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
